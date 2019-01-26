89eb4b8d0ba80784f47fa3d3a7d2f92305120bbe
Dan Caddigan <goldcaddy77@gmail.com>
2019-01-26 12:35:03 -0500

    fix(bugs): fix bugs introduced by previous commits

--- src/torm/createConnection.ts
+++ src/torm/createConnection.ts
@@ -2,35 +2,13 @@ import { ConnectionOptions, createConnection } from 'typeorm';
 
 import { SnakeNamingStrategy } from './SnakeNamingStrategy';
 
-const BASE_DB_CONFIG = {
-  cli: {
-    entitiesDir: 'src/models',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber'
-  },
-  database: process.env.TYPEORM_DATABASE,
-  entities: process.env.TYPEORM_ENTITIES || ['src/**/*.model.ts'],
-  host: process.env.TYPEORM_HOST || 'localhost',
-  logger: 'advanced-console',
-  logging: process.env.TYPEORM_LOGGING || 'all',
-  migrations: ['src/migration/**/*.ts'],
-  namingStrategy: new SnakeNamingStrategy(),
-  password: process.env.TYPEORM_PASSWORD,
-  port: parseInt(process.env.TYPEORM_PORT || '', 10) || 5432,
-  subscribers: ['src/**/*.model.ts'],
-  synchronize:
-    typeof process.env.TYPEORM_SYNCHRONIZE !== 'undefined'
-      ? process.env.TYPEORM_SYNCHRONIZE
-      : process.env.NODE_ENV === 'development',
-  type: 'postgres',
-  username: process.env.TYPEORM_USERNAME
-};
-
 export const createDBConnection = (dbOptions: Partial<ConnectionOptions> = {}) => {
   const config = {
-    ...BASE_DB_CONFIG,
+    ...getBaseConfig(),
     ...dbOptions
   };
+
+  // console.log('config: ', config);
 
   if (!config.database) {
     throw new Error("createConnection: 'database' is required");
@@ -51,3 +29,29 @@ export const mockDBConnection = (dbOptions: Partial<ConnectionOptions> = {}) => 
     type: 'sqlite'
   } as any);
 };
+
+function getBaseConfig() {
+  return {
+    cli: {
+      entitiesDir: 'src/models',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber'
+    },
+    database: process.env.TYPEORM_DATABASE,
+    entities: process.env.TYPEORM_ENTITIES || ['src/**/*.model.ts'],
+    host: process.env.TYPEORM_HOST || 'localhost',
+    logger: 'advanced-console',
+    logging: process.env.TYPEORM_LOGGING || 'all',
+    migrations: ['src/migration/**/*.ts'],
+    namingStrategy: new SnakeNamingStrategy(),
+    password: process.env.TYPEORM_PASSWORD,
+    port: parseInt(process.env.TYPEORM_PORT || '', 10) || 5432,
+    subscribers: ['src/**/*.model.ts'],
+    synchronize:
+      typeof process.env.TYPEORM_SYNCHRONIZE !== 'undefined'
+        ? process.env.TYPEORM_SYNCHRONIZE
+        : process.env.NODE_ENV === 'development',
+    type: 'postgres',
+    username: process.env.TYPEORM_USERNAME
+  };
+}
