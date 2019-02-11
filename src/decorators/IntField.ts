7a553681ef0555e3a1c758c9a04477d5c33e77f7
Dewey Jose <djose@indigoag.com>
2019-02-11 16:42:14 -0500

    removed unique

--- src/decorators/IntField.ts
+++ src/decorators/IntField.ts
@@ -5,12 +5,10 @@ import { composeMethodDecorators, MethodDecoratorFactory } from '../utils';
 
 interface IntFieldOptions {
   nullable?: boolean;
-  unique?: boolean;
 }
 
 export function IntField(args: IntFieldOptions = {}): any {
   const nullableOption = args.nullable === true ? { nullable: true } : {};
-  const uniqueOption = args.unique ? { unique: true } : {};
 
   // These are the 2 required decorators to get type-graphql and typeorm working
   const factories = [
@@ -21,8 +19,7 @@ export function IntField(args: IntFieldOptions = {}): any {
     }),
     Column({
       type: 'int',
-      ...nullableOption,
-      ...uniqueOption
+      ...nullableOption
     }) as MethodDecoratorFactory
   ];
 
