3bfc253c2cb7c95bcf83f7711d79c967cf1ffc42
Dewey Jose <djose@indigoag.com>
2019-02-11 15:49:51 -0500

    fix(graph Int to ts number) map Graph Int fields to number in generated code

---
+++ src/decorators/IntField.ts
@@ -0,0 +1,28 @@
+import { Field, Int } from 'type-graphql';
+import { Column } from 'typeorm';
+
+import { composeMethodDecorators, MethodDecoratorFactory } from '../utils';
+
+interface IntFieldOptions {
+  nullable?: boolean;
+  unique?: boolean;
+}
+
+export function IntField(args: IntFieldOptions = {}): any {
+  const nullableOption = args.nullable === true ? { nullable: true } : {};
+
+  // These are the 2 required decorators to get type-graphql and typeorm working
+  const factories = [
+    // We explicitly say string here because when we're metaprogramming without
+    // TS types, Field does not know that this should be a String
+    Field(type => Int, {
+      ...nullableOption
+    }),
+    Column({
+      type: 'int',
+      ...nullableOption
+    }) as MethodDecoratorFactory
+  ];
+
+  return composeMethodDecorators(...factories);
+}
