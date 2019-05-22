80f1489bebdd5a5a79c4d086d124398adb41a1f7
Peter Pritchard <ppritchard@indigoag.com>
2019-05-22 09:14:51 -0400

    add _default_ value for boolena and enum field annotations

--- src/decorators/BooleanField.ts
+++ src/decorators/BooleanField.ts
@@ -6,21 +6,25 @@ import { composeMethodDecorators, MethodDecoratorFactory } from '../utils';
 
 interface BooleanFieldOptions {
   nullable?: boolean;
+  default?: boolean;
 }
 
 export function BooleanField(args: BooleanFieldOptions = {}): any {
   const nullableOption = args.nullable === true ? { nullable: true } : {};
+  const defaultOption = args.default ? { default: args.default } : {};
 
   // These are the 2 required decorators to get type-graphql and typeorm working
   const factories = [
     // We explicitly say string here because when we're metaprogramming without
     // TS types, Field does not know that this should be a String
     Field(type => GraphQLBoolean, {
-      ...nullableOption
+      ...nullableOption,
+      ...defaultOption
     }),
     Column({
       type: 'boolean',
-      ...nullableOption
+      ...nullableOption,
+      ...defaultOption
     }) as MethodDecoratorFactory
   ];
 
