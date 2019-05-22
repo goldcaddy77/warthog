14a01a602400c8c4a829a3849c2d7abb69f68cd3
Peter Pritchard <ppritchard@indigoag.com>
2019-05-22 09:15:38 -0400

    add DateField annotation

---
+++ src/decorators/DateField.ts
@@ -0,0 +1,30 @@
+import { Field, GraphQLISODateTime } from 'type-graphql';
+import { Column } from 'typeorm';
+
+import { composeMethodDecorators, MethodDecoratorFactory } from '../utils';
+
+interface DateFieldOptions {
+  nullable?: boolean;
+  default?: Date;
+}
+
+export function DateField(args: DateFieldOptions = {}): any {
+  const nullableOption = args.nullable === true ? { nullable: true } : {};
+  const defaultOption = args.default ? { default: args.default } : {};
+
+  // These are the 2 required decorators to get type-graphql and typeorm working
+  const factories = [
+    // We explicitly say string here because when we're metaprogramming without
+    // TS types, Field does not know that this should be a String
+    Field(type => GraphQLISODateTime, {
+      ...nullableOption
+    }),
+    Column({
+      type: 'timestamp',
+      ...nullableOption,
+      ...defaultOption
+    }) as MethodDecoratorFactory
+  ];
+
+  return composeMethodDecorators(...factories);
+}
