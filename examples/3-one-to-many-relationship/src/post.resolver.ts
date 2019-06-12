8580e01ee323e7b3271aab77e685fb9dca069983
Dan Caddigan <goldcaddy77@gmail.com>
2019-06-12 13:46:28 -0400

    chore(prettier): change to 100 length and prettify existing files

--- examples/3-one-to-many-relationship/src/post.resolver.ts
+++ examples/3-one-to-many-relationship/src/post.resolver.ts
@@ -4,7 +4,13 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 
 import { BaseContext, BaseResolver, StandardDeleteResponse } from '../../../src';
-import { PostCreateInput, PostUpdateArgs, PostWhereArgs, PostWhereInput, PostWhereUniqueInput } from '../generated';
+import {
+  PostCreateInput,
+  PostUpdateArgs,
+  PostWhereArgs,
+  PostWhereInput,
+  PostWhereUniqueInput
+} from '../generated';
 
 import { Post } from './post.model';
 import { User } from './user.model';
@@ -42,7 +48,10 @@ export class PostResolver extends BaseResolver<Post> {
   }
 
   @Mutation(returns => Post)
-  async updatePost(@Args() { data, where }: PostUpdateArgs, @Ctx() ctx: BaseContext): Promise<Post> {
+  async updatePost(
+    @Args() { data, where }: PostUpdateArgs,
+    @Ctx() ctx: BaseContext
+  ): Promise<Post> {
     return this.update(data, where, ctx.user.id);
   }
 
