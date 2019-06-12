fac07da9f386a6ab3e177147e248d77dd9336841
Dan Caddigan <goldcaddy77@gmail.com>
2019-06-12 13:33:56 -0400

    fix(context): allow async context getter (#117)

--- examples/2-complex-example/src/server.ts
+++ examples/2-complex-example/src/server.ts
@@ -12,18 +12,24 @@ interface Context extends BaseContext {
   };
 }
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export function getServer(AppOptions = {}, dbOptions = {}) {
   return new Server<Context>(
     {
       // Inject a fake user.  In a real app you'd parse a JWT to add the user
-      context: request => {
-        return {
+      context: async request => {
+        // allows asynchronous resolution of user (or other items you want to put in context)
+        await sleep(500);
+        return Promise.resolve({
           user: {
             email: 'admin@test.com',
             id: 'abc12345',
             permissions: ['user:read', 'user:update', 'user:create', 'user:delete', 'photo:delete']
           }
-        };
+        });
       },
       introspection: true,
       logger: customLogger,
