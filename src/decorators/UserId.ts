b571c8bef9dde0551ba62cd8872445276a6d6f43
Dan Caddigan <goldcaddy77@gmail.com>
2019-06-16 20:04:45 -0400

    feat(decorators): adds UserId decorator (#125)

---
+++ src/decorators/UserId.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator } from 'type-graphql';
+
+export function UserId(): ParameterDecorator {
+  return createParamDecorator(({ context }: { context: { user?: { id?: string } } }) => {
+    if (!context.user) {
+      throw new Error('`user` attribute not found on context');
+    }
+    if (!context.user.id) {
+      throw new Error('`user` attribute does not contain an `id`');
+    }
+    return context.user.id;
+  });
+}
