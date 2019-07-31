c60071e7e088f33b3784b3134890520392acf6c0
Dan Caddigan <goldcaddy77@gmail.com>
2019-07-30 23:49:53 -0400

    feat(test): move under src folder (#181)
    * feat(test): move under src folder
    
    * fix(tests): move into src folder so that we dont have TS compile issues
    
    * feat(test): move under src folder

--- src/decorators/Fields.ts
+++ src/decorators/Fields.ts
@@ -3,6 +3,18 @@ import { createParamDecorator } from 'type-graphql';
 
 export function Fields(): ParameterDecorator {
   return createParamDecorator(({ info }) => {
-    return Object.keys(graphqlFields(info));
+    // This object will be of the form:
+    //   rawFields {
+    //     baseField: {},
+    //     association: { subField: "foo"}
+    //   }
+    // We need to pull out items with subFields
+    const rawFields = graphqlFields(info);
+
+    const scalars = Object.keys(rawFields).filter(item => {
+      return Object.keys(rawFields[item]).length === 0;
+    });
+
+    return scalars;
   });
 }
