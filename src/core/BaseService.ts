c7dc24a23278255223f33f4ad74581d9150b4dad
ikoenigsknecht <ikoenigsknecht@users.noreply.github.com>
2019-12-11 22:10:36 -0500

    fix(service): Fix querying for a column on multiple conditions (#278)
    * Fix querying for a column on multiple conditions
    
    * Add test case

--- src/core/BaseService.ts
+++ src/core/BaseService.ts
@@ -95,13 +95,20 @@ export class BaseService<E extends BaseModel> {
 
     if (Object.keys(where).length) {
       // where is of shape { userName_contains: 'a' }
-      Object.keys(where).forEach(k => {
+      Object.keys(where).forEach((k: string, i: number) => {
+        const paramKey = BaseService.buildParamKey(i);
         const key = k as keyof W; // userName
         const parts = key.toString().split('_'); // ['userName', 'contains']
         const attr = parts[0]; // userName
         const operator = parts.length > 1 ? parts[1] : 'eq'; // contains
 
-        qb = addQueryBuilderWhereItem(qb, attr, this.attrToDBColumn(attr), operator, where[key]);
+        qb = addQueryBuilderWhereItem(
+          qb,
+          paramKey,
+          this.attrToDBColumn(attr),
+          operator,
+          where[key]
+        );
       });
     }
 
@@ -212,4 +219,6 @@ export class BaseService<E extends BaseModel> {
   attrToDBColumn = (attr: string): string => {
     return `"${this.klass}"."${this.columnMap[attr]}"`;
   };
+
+  static buildParamKey = (i: number): string => `param${i}`;
 }
--- src/torm/operators.ts
+++ src/torm/operators.ts
@@ -2,7 +2,7 @@ import { SelectQueryBuilder } from 'typeorm';
 
 export function addQueryBuilderWhereItem<E>(
   qb: SelectQueryBuilder<E>,
-  dbColumn: string,
+  valueKey: string,
   columnWithAlias: string,
   operator: string,
   value: any
@@ -12,31 +12,31 @@ export function addQueryBuilderWhereItem<E>(
       if (value === null) {
         return qb.andWhere(`${columnWithAlias} IS NULL`);
       }
-      return qb.andWhere(`${columnWithAlias} = :${dbColumn}`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} = :${valueKey}`, { [valueKey]: value });
     case 'not':
-      return qb.andWhere(`${columnWithAlias} != :${dbColumn}`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} != :${valueKey}`, { [valueKey]: value });
     case 'lt':
-      return qb.andWhere(`${columnWithAlias} < :${dbColumn}`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} < :${valueKey}`, { [valueKey]: value });
     case 'lte':
-      return qb.andWhere(`${columnWithAlias} <= :${dbColumn}`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} <= :${valueKey}`, { [valueKey]: value });
     case 'gt':
-      return qb.andWhere(`${columnWithAlias} > :${dbColumn}`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} > :${valueKey}`, { [valueKey]: value });
     case 'gte':
-      return qb.andWhere(`${columnWithAlias} >= :${dbColumn}`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} >= :${valueKey}`, { [valueKey]: value });
     case 'in':
       // IN (:... is the syntax for exploding array params into (?, ?, ?) in QueryBuilder
-      return qb.andWhere(`${columnWithAlias} IN (:...${dbColumn})`, { [dbColumn]: value });
+      return qb.andWhere(`${columnWithAlias} IN (:...${valueKey})`, { [valueKey]: value });
     case 'contains':
-      return qb.andWhere(`LOWER(${columnWithAlias}) LIKE :${dbColumn}`, {
-        [dbColumn]: `%${value}%`
+      return qb.andWhere(`LOWER(${columnWithAlias}) LIKE :${valueKey}`, {
+        [valueKey]: `%${value}%`
       });
     case 'startsWith':
-      return qb.andWhere(`LOWER(${columnWithAlias}) LIKE :${dbColumn}`, {
-        [dbColumn]: `${value}%`
+      return qb.andWhere(`LOWER(${columnWithAlias}) LIKE :${valueKey}`, {
+        [valueKey]: `${value}%`
       });
     case 'endsWith':
-      return qb.andWhere(`LOWER(${columnWithAlias}) LIKE :${dbColumn}`, {
-        [dbColumn]: `%${value}`
+      return qb.andWhere(`LOWER(${columnWithAlias}) LIKE :${valueKey}`, {
+        [valueKey]: `%${value}`
       });
     default:
       throw new Error(`Can't find operator ${operator}`);
