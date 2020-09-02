647db4ab379a878dd0cbab28e929ea111b7b7f2d
Dan Caddigan <goldcaddy77@gmail.com>
2020-09-01 22:19:52 -0400

    feat(decorators): adds endpoint decorators

---
+++ src/decorators/CreateManyMutation.ts
@@ -0,0 +1,3 @@
+import { Arg, Args, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
---
+++ src/decorators/DeleteMutation.ts
@@ -0,0 +1,5 @@
+import { Arg, Args, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+
+// @Mutation(() => StandardDeleteResponse)
